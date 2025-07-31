d83ea2ac728b89b9112b0ec8a1e2f90ad3d4ad60
Cheng-Yen Hsiao <128729745+Hsiao1027@users.noreply.github.com>
2025-07-31 19:40:37 +0800

    Update eval.py

--- eval.py
+++ eval.py
@@ -1,11 +1,11 @@
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    model = YOLO('/root/workspace/pdf2/yolov12/runs/detect/train14/weights/best.pt')
+    model = YOLO('yolov12n.pt')
     metrics = model.val(
         batch=4,
         imgsz=960,
-        data='./mix_dataset.yaml',   
+        data='.dataset.yaml',   
         split='val'                                            
     )
   
